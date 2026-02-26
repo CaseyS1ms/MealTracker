42373ff13397e339ae8468d63b7f7d48dccf841d
caseysims <caseysims07@gmail.com>
2026-02-26 15:35:24 +0000

    initial commit

---
+++ src/test/java/com/caseysims/mealtracker/MealTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caseysims.mealtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

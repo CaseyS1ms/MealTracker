ddb9c2445098bbd275e349ca37c367177e7e9fb1
caseysims <caseysims07@gmail.com>
2026-02-26 16:19:49 +0000

    getMeal endpoint

--- src/main/java/com/caseysims/mealtracker/controller/MealController.java
+++ src/main/java/com/caseysims/mealtracker/controller/MealController.java
@@ -1,9 +1,25 @@
 package com.caseysims.mealtracker.controller;
 
+import com.caseysims.mealtracker.entity.Meal;
+
+import com.caseysims.mealtracker.service.MealService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MealController
 {
+
+    @Autowired
+    MealService mealService;
+
+    @GetMapping("getMeals")
+    public List<Meal> getMeals()
+    {
+        return mealService.getMeals();
+    }
 
 }
